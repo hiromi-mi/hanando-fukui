55894e5b933cd5f525b63b008b3189832c2ca30d
hiromi-mi <hiromi-mi@cat.zaq.jp>
2019-08-16 21:28:40 +0900

    add test3

--- test3.cpp
+++ test3.cpp
@@ -1,3 +1,11 @@
+/*
+$ ./hanando -r -f -cpp test3.cpp > test3.s && gcc -g test3.s -o test3 && ./test3
+
+Output:
+   int: 357
+   char: 101
+*/
+
 template<typename T> T add(T a, T b) {
    T c;
    c = a+b;
