bd85879e97244395d4a35fed43180ad88c0447bb
hiromi-mi <hiromi-mi@cat.zaq.jp>
2019-12-20 17:23:37 +0900

    test5, 6 をC言語ベースに移行

--- test.c
+++ test.c
@@ -1,6 +1,8 @@
 // test.c
 
 #include "main.h"
+extern int *stderr;
+extern int *stdout;
 
 // See hoc_nyan/test/test.c
 #define EXPECT(expr1, expr2) { \
@@ -8,7 +10,7 @@
    e1 = (expr1); \
    e2 = (expr2); \
    if (e1 == e2) { \
-      fprintf(stderr, "Line %d: %s: OK\n", __LINE__, #expr1); \
+      fprintf(stdout, "Line %d: %s: OK %d\n", __LINE__, #expr1, e2); \
    } else { \
       fprintf(stderr, "Line %d: %s: Expr1 %d, Expr2 %d\n", __LINE__, #expr1, e1, e2); \
       retval++; \
@@ -35,6 +37,7 @@ int main(void) {
    EXPECT(1*9, 9);
    EXPECT(-1*9, -9);
    EXPECT(-2*-9, 18);
+   EXPECT(-2/-9, 0);
    EXPECT((9-11)*34, -68);
    EXPECT(255*2, 510);
 
@@ -48,12 +51,27 @@ int main(void) {
    EXPECT(!(2==2), 0);
    EXPECT(!(-5==2), 1);
 
+   /* test5,6*/
+   EXPECT(6%3, 0);
+   // C と Python で異なる仕様
+   EXPECT(-1%3, -1);
+   EXPECT(5%4, 1);
+   EXPECT(1^0, 1);
+   EXPECT(0^1, 1);
+   EXPECT(3^2, 1);
+   EXPECT(6|3, 7);
+   EXPECT(1|0, 1);
+   EXPECT(1&0, 0);
+   EXPECT(1<<1, 2);
+   EXPECT(1<<0, 1);
+   EXPECT(2<<4, 32);
+   EXPECT(9>>1, 4);
    //EXPECT((char)255+2, 1);-
    /*
-	sh testfdef.sh 'int main() {int; return 2;}' 2
-	sh testfdef.sh 'int main() {1; 2; 3; 4; 5; 6; 7; 8; return 2;}' 2
-	sh testfdef.sh "int main(){{return 3;}}" 3
-        */
+      sh testfdef.sh 'int main() {int; return 2;}' 2
+      sh testfdef.sh 'int main() {1; 2; 3; 4; 5; 6; 7; 8; return 2;}' 2
+      sh testfdef.sh "int main(){{return 3;}}" 3
+      */
    fprintf(stderr, "The number of errors are: %d\n", retval);
    return retval;
 }
